cc170710f15eb3785ea17f16609db7ba972beac1
Administrator <skyestzhang@163.com>
2019-10-15 17:10:01 +0800

    fix database pwd:

--- conf/db.js
+++ conf/db.js
@@ -2,7 +2,7 @@ module.exports= {
     host: '106.12.40.54',
     port: 51012,
     user: 'postgres',
-    password: '20191012',
+    password: '20191012bgib',
     database: 'advertise',
     max: 10000,
     idleTimeoutMillis: 30000,
